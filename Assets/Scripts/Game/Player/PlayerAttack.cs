b7238a11edb661142e5c55386d60fd95a39dfaa6
KaNaBiNoL <tassadar1624@gmail.com>
2022-08-29 21:12:06 +0300

    Player, shooting and movement

---
+++ Assets/Scripts/Game/Player/PlayerAttack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace TDS.Game.Player
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+        [SerializeField] private GameObject _bulletPrefab;
+        [SerializeField] private Transform _shotPosition;
+        [SerializeField] private float _fireDelay = 0.3f;
+
+        private float _timer;
+        private Transform _cachedTransform;
+
+        private void Awake()
+        {
+            _cachedTransform = transform;
+        }
+
+        private void Update()
+        {
+            if (CanAttack())
+            {
+                Attack();
+            }
+
+            TickTimer();
+        }
+
+        private void Attack()
+        {
+            Instantiate(_bulletPrefab, _shotPosition.position, _cachedTransform.rotation);
+            _timer = _fireDelay;
+        }
+
+        private bool CanAttack()
+        {
+            return Input.GetButton("Fire1") && _timer <= 0;
+        }
+
+        private void TickTimer()
+        {
+            _timer -= Time.deltaTime;
+        }
+    }
+}
